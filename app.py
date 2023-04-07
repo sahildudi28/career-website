2d507d4ae51525bbc03c74784104cc48581b91fb
sahildudi <129891019+sahildudi28@users.noreply.github.com>
2023-04-07 11:03:45 +0000

    added dynamic data and route

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+STAT = [{
+  'id': 'batting',
+  'style': 'right hand batsman'
+}, {
+  'id': 'bowling',
+  'style': 'right arm medium'
+}]
 
 
 @app.route("/")
 def hello():
-  return render_template("home.html")
+  return render_template("home.html", stat=STAT, my_name='Sahil Dudi')
+
+
+@app.route("/api/stat")
+def stats():
+  return jsonify(STAT)
 
 
 if __name__ == "__main__":
